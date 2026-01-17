ec8cc568bb1255d28c36ac09e66f9c97b262d56f
Lister Solijon <157126682+Solijon113@users.noreply.github.com>
2026-01-17 23:42:18 +0800

    Add testing paragraph to welcome page
    Added a new paragraph for testing purposes.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
 
     <p style="text-align:center;">Hello, My Name is Lister Y. Solijon</p>
     <p style="text-align:center;">22 years old</p>
-    <p style="text-align: center;">I live in Cagayan de Oro City, Misamis Oriental</p>
+    <p style="text-align:center;">I live in Cagayan de Oro City, Misamis Oriental</p>
+    <p style="text-align:center;">Testing</p>
 </body>
 </html>
