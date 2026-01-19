568d9342d4fa0376f6facc711690cbaa60a7cc73
tbdotstagoloan-ship-it <tbdotstagoloan@gmail.com>
2026-01-20 00:08:55 +0800

    Working on other section

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\MyPortfolioController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/my-portfolio', [MyPortfolioController::class, 'index']);
